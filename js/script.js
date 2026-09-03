d3dbe6d16f443f374bf03d21f6aef621359a8bf2
Ray-han25 <abdulllahmutazab@gmail.com>
2026-09-03 23:16:16 +0600

    My new add

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 AOS.init();
+$('.feedback_silder').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow :'  <button class="prvaaarr sameButton"> <i class="fa-solid fa-arrow-left"></i></button>',
+  nextArrow:'<button class="nextaaarr sameButton"> <i class="fa-solid fa-arrow-right"></i></button>'
+
+});
