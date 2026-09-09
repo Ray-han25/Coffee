15c68a3ae51325e55022922e4b43618fb4baade0
Ray-han25 <abdulllahmutazab@gmail.com>
2026-09-09 21:54:04 +0600

    add resgistar

--- js/script.js
+++ js/script.js
@@ -11,3 +11,139 @@ $('.feedback_silder').slick({
   nextArrow:'<button class="nextaaarr sameButton"> <i class="fa-solid fa-arrow-right"></i></button>'
 
 });
+// ----------------------all dom
+const popDiv= document.querySelector('.sing_singuP')
+const Sing_up= document.querySelector('.Sing_up')
+const Login= document.querySelector('.Login')
+// ----------------------useName----------------------------------
+const UseName =document.querySelector('.Use_name')
+const Email =document.querySelector('.Email')
+const password =document.querySelector('.password')
+const confirmPassword =document.querySelector('.Confirm_password')
+// _---------------------------------------------------------all input -here singup input here
+const useN=document.querySelector('.useN')
+const emailN=document.querySelector('.emailN')
+const passwordN=document.querySelector('.passwordN')
+const confirmN=document.querySelector('.confirmN')
+// ------------------------------------------------------------------------------
+// ----------------------------login-input here----------------------------------------------
+const longu=document.querySelector('.longu')
+const logPass=document.querySelector('.logPass')
+ 
+// ------------------login-all div here
+const logingUsediv=document.querySelector('.loging_usediv')
+const logPassDiv=document.querySelector('.logPassDiv')
+
+
+
+
+// ---------------------------Login-------------------------------------
+const hendelbutton1=()=>{  
+ popDiv.style.display='flex'
+ Sing_up.style.display='none'
+ Login.style.display='block'
+ document.body.style.overflow='hidden'
+}
+// ----------------------------------singup---------------------------
+const hendelbutton2=()=>{
+ popDiv.style.display='flex'
+ Login.style.display='none'
+ Sing_up.style.display='block'
+ document.body.style.overflow='hidden'
+}
+
+// --------------Login t0 singup and singup to login
+const hendelbutton3=()=>{
+ Login.style.display='none'
+ Sing_up.style.display='block'
+}
+const hendelbutton4=()=>{
+ Sing_up.style.display='none'
+ Login.style.display='block'
+}
+
+// ---------------------hendelsing up--------------------------
+const hendelSign=(event)=>{
+ event.preventDefault()
+if(UseName.value=='') useN.classList.add('same_input')
+if(Email.value=='')emailN.classList.add('same_input')
+if(password.value=='') passwordN.classList.add('same_input')
+if(confirmPassword.value=='')  confirmN.classList.add('same_input')
+if(password.value!==confirmPassword.value) return confirmN.classList.add('same_input') 
+  const url ='https://api.freeapi.app/api/v1/users/register';
+fetch(url,  {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+   body: JSON.stringify({
+    email: Email.value ,
+    password:password.value,
+    role: 'ADMIN',
+    username: UseName.value
+  })
+
+
+
+})
+.then((res)=>{
+  if(!res.ok){
+    throw new Error(res.statusText)
+  }
+else{
+  return res.json()
+}
+})
+.then((date)=>{
+  console.log(date.message)
+  Toastify({
+  text: date.message,
+  duration: 3000,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  style: {
+    background: "linear-gradient(to right, #00b09b, #96c93d)",
+  },
+}).showToast();
+})
+ .catch((err)=>{
+  console.log(err.message)
+   Toastify({
+  text: err.message,
+  duration: 3000,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "center", // `left`, `center` or `right`
+  style: {
+    background: "red",
+  },
+}).showToast();
+ })
+}
+// -------------------------add evanlesenar-------------------------
+emailN.addEventListener('input',()=>{
+  emailN.classList.remove('same_input')
+})
+useN.addEventListener('input',()=>{
+  useN.classList.remove('same_input')
+})
+passwordN.addEventListener('input',()=>{
+  passwordN.classList.remove('same_input')
+})
+confirmN.addEventListener('input',()=>{
+  confirmN.classList.remove('same_input')
+})
+// --------------------------hendelLogin
+
+const hendelLogin=(event)=>{
+event.preventDefault()
+if(longu.value=='') logingUsediv.classList.add('same_input')
+if(logPass.value=='') logPassDiv.classList.add('same_input')
+}
+logingUsediv.addEventListener('input',()=>{
+  logingUsediv.classList.remove('same_input')
+})
+logPassDiv.addEventListener('input',()=>{
+  logPassDiv.classList.remove('same_input')
+})
